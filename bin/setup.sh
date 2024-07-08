4e43ca38e2160b6ca9a12d793055d594ac4ee38b
Moxvallix <moxvallix@gmail.com>
2024-07-08 16:00:48 +0930

    chore: update mkdocs version, add automatic light / dark mode

--- bin/setup.sh
+++ bin/setup.sh
@@ -7,6 +7,6 @@ echo "Update your .env file to match your projects specifics"
 echo ".env is not commited into your repo, share it by other *secure* means than via git."
 echo "For help with deploying changes to .env to production see DEPLOYMENT.md"
 echo ">>>>>>> SETUP LOCAL HOMEPAGE <<<<<<<<<<"
-cp /docs/index.md.example /docs/index.md
+cp ./docs/index.md.example ./docs/index.md
 echo "/docs/index.md.example copied to /docs/index.md"
 echo "Review and edit /docs/index.md to taste, this is the welcome page for your handbook."
